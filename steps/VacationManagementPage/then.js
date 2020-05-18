767c26a80e1f1af0b2056ae4b181178aadc7f06a
handresc1127 <h.andresc1127@gmail.com>
2020-05-17 23:21:27 -0500

    update Then and When

--- steps/VacationManagementPage/then.js
+++ steps/VacationManagementPage/then.js
@@ -1,5 +1,6 @@
 import { Then } from "cucumber";
 import homePage from "../../pages/Home";
+import listPage from "../../pages/List";
 void
 
 Then('Verify than is logged-in the homepage', () => { 
@@ -9,11 +10,16 @@ Then('Verify than is logged-in the homepage', () => {
 });
 
 
-Then('Verify the before user exist', () => { 
+Then('Verify the before user {string} exist', leader => { 
+    listPage.goTo();
+    listPage.verifyUser(leader);
 });
-Then('Return user row', () => { 
+Then('Return user row {string}', leader => { 
+    listPage.returnUser(leader);
 });
-Then('Delete before user', () => { 
+Then('Delete before user {string}',leader => { 
+    listPage.deleteUser(leader);
 });
-Then('Verify user was deleted', () => { 
+Then('Verify user was deleted {string}', leader => { 
+    listPage.verifyNoUser(leader);
 });
--- steps/VacationManagementPage/when.js
+++ steps/VacationManagementPage/when.js
@@ -1,5 +1,7 @@
 import { When } from "cucumber";
 import loginPage from "../../pages/Login";
+import creationPage from "../../pages/Creation"
+
 void
 When('Login into Vacations Management', () => { 
     loginPage.login("gap-automation-test@mailinator.com","12345678");
@@ -10,16 +12,25 @@ When('Go to option create a new user', () => {
 });
 
 When('Create the user {string}', fullName => { 
+    creationPage.setFullName(fullName);
 });
 
 When('Set the identification {string}', identification => { 
+    creationPage.setIdentification(identification);
 });
 
 When('Set date-starts with {string}', startDate => { 
+    creationPage.setDateStarts(startDate);
 });
 
 When('Set the leader field with {string}', leader => { 
+    creationPage.setLeader(leader);
 });
 
 When('Save the user', () => { 
+    creationPage.userCreateSubmit();
+});
+
+When('Set the email address with {string}', email => { 
+    creationPage.setEmail(email);
 });
